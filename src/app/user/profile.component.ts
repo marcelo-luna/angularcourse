202dc81685ef418adcb90dd7b72d340ee0cdc7a0
MarceloLuna <marcelo.1.luna@gmail.com>
2020-05-24 22:50:39 +0200

    module 7/7

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,37 +1,44 @@
 import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, Validator } from '@angular/forms'
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html',
+  styles: [`
+    em {float:right; color# E05C65; padding-left: 10px;}
+    .error input { 
+
+    }
+  `]
 })
 export class ProfileComponent implements OnInit {
 
-   profileForm: FormGroup;
+  profileForm: FormGroup;
 
-   constructor(private authService: AuthService, private router: Router) {
-     
-   }
-  
-  ngOnInit(): void {
-    console.log(this.authService.currentUser.firstName);
-  //  let firstName = new FormControl(this.authService.currentUser.firstName);
-  //  let lastName = new FormControl(this.authService.currentUser.lastName);
-  let firstName = new FormControl(this.authService.currentUser.firstName);
-  let lastName = new FormControl(this.authService.currentUser.lastName);
-   this.profileForm = new FormGroup({
-    firstName: firstName,
-     lastName: lastName
-   })
+  constructor(private authService: AuthService, private router: Router, private validator: Validator) {
+
   }
 
-  cancelClick(){
+  ngOnInit(): void {
+
+    if (this.profileForm.valid){
+      let firstName = new FormControl(this.authService.currentUser.firstName, this.validator.);
+      let lastName = new FormControl(this.authService.currentUser.lastName);
+      this.profileForm = new FormGroup({
+        firstName: firstName,
+        lastName: lastName
+      })
+    }
+  }
+
+  cancelClick() {
     this.router.navigate(['events']);
   }
 
-  saveclick(formsValue){
-    this.authService.updateCurrentUser(formsValue.firstName, formsValue.lastName);
+  saveclick(formsValue) {
+
+    this.authService.updateCurrentUser(formsValue.value.firstName, formsValue.value.lastName);
   }
 
 }
